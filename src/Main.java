f80d569f1c59b1e81d960365e8234ee7ba13a942
Gravinato <dimychmen@yandex.ru>
2022-07-26 16:56:13 +0300

    dz 1.1 1+

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args){
+        // Задание 1
+
+        int apple = 5;
+        byte banana = 3;
+        short lemon = 11;
+        long orange = 157L;
+        double menWeight = 47.5;
+        float womanWeight = 107.3f;
+        boolean menHeavy = menWeight > 30;
+        char bar = 11;
+        System.out.println (menHeavy);
+
+
+    }
 }
